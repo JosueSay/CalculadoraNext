850b39fb6e379ec4a95eb82efcd8b7ab7664446f
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-05-17 02:29:35 -0600

    fix: double click of button with keyboard and mouse

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import './calculadora.css';
 function Home() {
   const [displayText, setDisplayText] = useState('');
   const [errorDisplay, setErrorDisplay] = useState(false);
+  const [activeButton, setActiveButton] = useState(null);
 
   const buttonDataHorizontal = useMemo(() => [
     { title: 'AC', className: 'button-operator' },
@@ -21,7 +22,7 @@ function Home() {
     { title: '1', className: 'button-number' },
     { title: '2', className: 'button-number' },
     { title: '3', className: 'button-number' },
-    { title: '%', className: 'button-number' },
+    { title: '+/-', className: 'button-operator' },
     { title: '0', className: 'button-number' },
     { title: '.', className: 'button-number' },
   ], []);
@@ -33,65 +34,95 @@ function Home() {
     { title: '=', className: 'button-equals' },
   ], []);
 
-  const clickButton = (text) => {
-    console.log(`Presionaste el botón ${text}`);
+  const clickButton = (text, source, className) => {
+    console.log(`Presionaste el botón ${text} con ${source}`);
+    if (source === 'teclado') {
+      setActiveButton(text);
+      setTimeout(() => setActiveButton(null), 100);
+    }
+  
     if (text === 'ERROR') {
       setErrorDisplay(true);
       setDisplayText(text);
     } else {
       setErrorDisplay(false);
-      setDisplayText(text);
+      // Verifica si el botón es de tipo operador antes de actualizar el displayText
+      if (!className.includes('button-operator') && !className.includes('button-equals')) {
+        setDisplayText(prevText => prevText + text);
+      }      
     }
-  };
-
-  const handleKeyDown = (event) => {
-    const key = event.key;
-    const buttonTitles = buttonDataHorizontal.concat(buttonDataVertical).map(button => button.title);
-
-    if (buttonTitles.includes(key)) {
-      clickButton(key);
-    }
-  };
+  };  
 
   useEffect(() => {
     const handleKeyDown = (event) => {
       const key = event.key;
-      const buttonTitles = buttonDataHorizontal.concat(buttonDataVertical).map(button => button.title);
-      if (key === 'Enter') {
-        clickButton('=');
-      } else if (key === 'Backspace') {
-        clickButton('C');
-      } else if (key === 'Delete') {
-        clickButton('AC');
-      } else if (buttonTitles.includes(key)) {
-        clickButton(key);
+      // Obtén tanto el título como la clase del botón correspondiente a la tecla presionada
+      const buttonInfo = buttonDataHorizontal.concat(buttonDataVertical).find(button => button.title === key);
+    
+      if (buttonInfo) { // Solo actuar si se encuentra un botón correspondiente
+        event.preventDefault();
+        clickButton(buttonInfo.title, 'teclado', buttonInfo.className);
       }
+    
+      // Manejo especial para teclas específicas
+      if (key === 'Enter') {
+        event.preventDefault();
+        const equalsButton = buttonDataVertical.find(button => button.title === '=');
+        if (equalsButton) {
+          clickButton('=', 'teclado', equalsButton.className);
+        }
+      } else if (key === 'Backspace') {
+        const cButton = buttonDataHorizontal.find(button => button.title === 'C');
+        if (cButton) {
+          clickButton('C', 'teclado', cButton.className);
+        }
+      } else if (key === 'Delete') {
+        const acButton = buttonDataHorizontal.find(button => button.title === 'AC');
+        if (acButton) {
+          clickButton('AC', 'teclado', acButton.className);
+        }
+      }
+    };
+    
+
+    const handleKeyUp = (event) => {
+      setActiveButton(null);
     };
 
     document.addEventListener('keydown', handleKeyDown);
-
+    document.addEventListener('keyup', handleKeyUp);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
     };
   }, [buttonDataHorizontal, buttonDataVertical]);
 
-
   return (
-    <main className='calculadora-container' tabIndex='0' onKeyDown={handleKeyDown}>
+    <main className='calculadora-container' tabIndex='0'>
       <div className='display-calculadora'>
         <Display text={displayText} className={errorDisplay ? 'display-text' : 'display-number'} />
       </div>
       <div className='button-calculadora'>
-        <div className='buttons-calculator-horizontal'>
-          {buttonDataHorizontal.map(({ title, className }) => (
-            <Button key={title} title={title} onClick={() => clickButton(title)} className={className} />
-          ))}
-        </div>
-        <div className='buttons-calculator-vertical'>
-          {buttonDataVertical.map(({ title, className }) => (
-            <Button key={title} title={title} onClick={() => clickButton(title)} className={className} />
-          ))}
-        </div>
+      <div className='buttons-calculator-horizontal'>
+  {buttonDataHorizontal.map(({ title, className }) => (
+    <Button
+      key={title}
+      title={title}
+      onClick={() => clickButton(title, 'ratón', className)}
+      className={`${className} ${activeButton === title ? 'button-active' : ''}`}
+    />
+  ))}
+</div>
+<div className='buttons-calculator-vertical'>
+  {buttonDataVertical.map(({ title, className }) => (
+    <Button
+      key={title}
+      title={title}
+      onClick={() => clickButton(title, 'ratón', className)}
+      className={className}
+    />
+  ))}
+</div>
       </div>
     </main>
   );
